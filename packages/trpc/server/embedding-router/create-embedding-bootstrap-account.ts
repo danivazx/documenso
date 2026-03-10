127bd6b2392a5419371acd0e94ea587cd998ee4d
danivazx <danivazx@gmail.com>
2026-03-10 08:35:05 +0100

    feat: add envvars

--- packages/trpc/server/embedding-router/create-embedding-bootstrap-account.ts
+++ packages/trpc/server/embedding-router/create-embedding-bootstrap-account.ts
@@ -16,10 +16,10 @@ import {
   createEmbeddingBootstrapAccountMeta,
 } from './create-embedding-bootstrap-account.types';
 
-const EMBEDDING_BOOTSTRAP_SECRET_HEADER = 'x-documenso-internal-secret';
+const EMBEDDING_BOOTSTRAP_SECRET_HEADER = env('DOCUMENSO_EMBEDDING_BOOTSTRAP_SECRET') || '';
 const EMBEDDING_BOOTSTRAP_FALLBACK_SECRET = 'documenso-embedding-bootstrap-secret';
 const EMBEDDING_BOOTSTRAP_WEBHOOK_URL = 'http://localhost:4321/webhook/documenso';
-const EMBEDDING_BOOTSTRAP_WEBHOOK_SECRET = 'documenso-webhook-secret';
+const EMBEDDING_BOOTSTRAP_WEBHOOK_SECRET = env('DOCUMENSO_WEBHOOK_SECRET') || '';
 const EMBEDDING_BOOTSTRAP_WEBHOOK_TRIGGERS = [
   WebhookTriggerEvents.DOCUMENT_SIGNED,
   WebhookTriggerEvents.DOCUMENT_COMPLETED,
